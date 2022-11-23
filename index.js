8d9599e2acb8bd3e3ab3a462e35849c5f7206b40
jenna-lab <jennifer.sammy@student.moringaschool.com>
2022-11-23 23:42:57 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (num1, num2) => {
+  console.log((num1 = 2000));
+  console.log((num2 = 100));
+  return num1 / num2;
+};
+const square = (x) => {
+  console.log(x);
+  console.log(x);
+  return x * x;
+};
+const add = (a, b) => {
+  console.log(a);
+  console.log(b);
+  return a + b;
+};
